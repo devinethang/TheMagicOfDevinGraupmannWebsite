b4c46a086ed82da7c1e0ed1f509909c3b4bfb187
LAPTOP-BRVKRNBD\devin <devinethang@gmail.com>
2021-08-24 16:52:14 -0700

    Finished?

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
 commonNav.innerHTML = `
     <div class="top-nav">
-    <img class="customNavLogo" style="position: absolute; padding: 20px;" width="200" src="images/navLogo2.png"/>
+    <img class="customNavLogo" style="position: absolute; padding: 20px;" width="14%" src="images/navLogo2.png"/>
     <ul class="navigation">
-        <li class="customNavLogo2"><img width="200" src="images/UsesPhotos/navLogo2.png"/></li>
+        <li class="customNavLogo2"><img width="200" src="images/navLogo.png"/></li>
         <li><a href="index.html">Home</a></li>
-        <li><a href="shows_h3.html">Shows</a></li>
+        <li><a href="shows.html">Shows</a></li>
         <li><a href="press.html">Press&nbsp;Kit</a></li>
         <li><a href="about.html">About</a></li>
         <li><a href="Contact.html">Contact</a></li>
@@ -20,7 +20,7 @@ commonFooter.innerHTML = `  <div class="footer-entry">
 <h2>Contact Information</h2>
 <p>Email: <a href="mailto:DevinEthanG@gmail.com">DevinEthanG@gmail.com</a></p>
 <p>Phone Number: 425-361-6603 </p>
-<span class="Social"><a href="https://www.facebook.com/devin.graupmann" style="color: lightblue;" target="_blank">
+<span class="Social"><a href="https://www.facebook.com/The-Magic-of-Devin-Graupmann-598633606912247" style="color: lightblue;" target="_blank">
 <img src="images/UsedPhotos/facebook_logo.png" alt="The Magic of Devin Graupmann Facebook" width="50"></a></span>
 
 <p><small>Copyright &copy; 2021<span id="lblYear"></span>. Devin Graupmann. All rights reserved.</small></p>
